78582b7fbd16b2d9983ac3c17b0f21a26ecc7db6
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-10 14:34:10 +0300

    CS fixes

--- headers/utils.h
+++ headers/utils.h
@@ -1,5 +1,5 @@
-#ifndef librabbitmq_benchmark_utils_h
-#define librabbitmq_benchmark_utils_h
+#ifndef RABBITMQ_BENCHMARK_UTILS_H
+#define RABBITMQ_BENCHMARK_UTILS_H
 
 void die(const char *fmt, ...);
 
@@ -8,4 +8,4 @@ extern void die_on_amqp_error(amqp_rpc_reply_t x, char const *context);
 
 extern long long timeInMilliseconds(void);
 
-#endif
+#endif //RABBITMQ_BENCHMARK_UTILS_H
