4d1649215cc25309646accdd7685cb0337ebb77b
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-11 14:55:01 +0300

    Fixed cfg.h

--- headers/cfg.h
+++ headers/cfg.h
@@ -1,6 +1,8 @@
 #ifndef RABBITMQ_BENCHMARK_CONFIG_H
 #define RABBITMQ_BENCHMARK_CONFIG_H
 
+#include <libconfig.h>
+
 int cfg_init(config_t *cfg, const char *filename);
 int cfg_read(config_t *cfg, const char *filename);
 
