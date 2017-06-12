383bd2ff3c156846952b863f31078b4a1e3a327f
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-12 13:06:19 +0300

    Introduce rabbit_configure

---
+++ headers/rabbit_configure.h
@@ -0,0 +1,26 @@
+#ifndef RABBITMQ_BENCHMARK_RABBIT_CONFIGURE_H
+#define RABBITMQ_BENCHMARK_RABBIT_CONFIGURE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libconfig.h>
+
+#include "rabbit_instance.h"
+
+#define RABBIT_CONFIGURE_GET_ITEM_NAME(/* char * */ item, /* char * */ partname, /* const char * */ item_name) \
+	do { \
+		size_t len_total; \
+		len_total = strlen(partname) + strlen(item_name) + 1; \
+		item = calloc(len_total + 1, sizeof(char)); \
+		snprintf(item, len_total + 1, "%s.%s", partname, item_name); \
+	} while (0)
+
+void rcfg_get_address(config_t *cfg, char *partname, rabbit_instance *rmq);
+void rcfg_get_port(config_t *cfg, char *partname, rabbit_instance *rmq);
+void rcfg_get_vhost(config_t *cfg, char *partname, rabbit_instance *rmq);
+void rcfg_get_username(config_t *cfg, char *partname, rabbit_instance *rmq);
+void rcfg_get_password(config_t *cfg, char *partname, rabbit_instance *rmq);
+void rcfg_get_queue(config_t *cfg, char *partname, rabbit_instance *rmq);
+
+#endif //RABBITMQ_BENCHMARK_RABBIT_CONFIGURE_H
---
+++ rabbit_configure.c
@@ -0,0 +1,129 @@
+#include <string.h>
+#include <stdlib.h>
+#include <libconfig.h>
+
+#include "logger.h"
+#include "rabbit_configure.h"
+#include "rabbit_instance.h"
+
+void rcfg_get_address(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	const char *str_tmp = NULL;
+	size_t len_str = 0;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "address");
+
+	if (config_lookup_string(cfg, item, &str_tmp)) {
+		len_str = strlen(str_tmp) + 1;
+		rmq->address = calloc(len_str, sizeof(char));
+		strncpy(rmq->address, str_tmp, len_str - 1);
+	} else {
+		rmq->address = strndup("127.0.0.1", 9);
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ address, used default '127.0.0.1'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
+
+void rcfg_get_port(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	int port;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "port");
+
+	if (config_lookup_int(cfg, item, &port)) {
+		rmq->port = (short unsigned int) (port);
+	} else {
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ port, used default '5672'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
+
+void rcfg_get_vhost(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	const char *str_tmp = NULL;
+	size_t len_str = 0;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "vhost");
+
+	if (config_lookup_string(cfg, item, &str_tmp)) {
+		len_str = strlen(str_tmp) + 1;
+		rmq->vhost = calloc(len_str, sizeof(char));
+		strncpy(rmq->vhost, str_tmp, len_str - 1);
+	} else {
+		rmq->vhost = strndup("/", 1);
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ vhost, used default '/'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
+
+void rcfg_get_username(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	const char *str_tmp = NULL;
+	size_t len_str = 0;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "username");
+
+	if (config_lookup_string(cfg, item, &str_tmp)) {
+		len_str = strlen(str_tmp) + 1;
+		rmq->username = calloc(len_str, sizeof(char));
+		strncpy(rmq->username, str_tmp, len_str - 1);
+	} else {
+		rmq->username = strndup("guest", 5);
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ username, used default 'guest'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
+
+void rcfg_get_password(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	const char *str_tmp = NULL;
+	size_t len_str =0;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "password");
+
+	if (config_lookup_string(cfg, item, &str_tmp)) {
+		len_str = strlen(str_tmp) + 1;
+		rmq->password = calloc(len_str, sizeof(char));
+		strncpy(rmq->password, str_tmp, len_str - 1);
+	} else {
+		rmq->password = strndup("guest", 5);
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ password, used default 'guest'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
+
+void rcfg_get_queue(config_t *cfg, char *partname, rabbit_instance *rmq) {
+	const char *str_tmp = NULL;
+	size_t len_str =0;
+	char *item = NULL;
+
+	RABBIT_CONFIGURE_GET_ITEM_NAME(item, partname, "queue");
+
+	if (config_lookup_string(cfg, item, &str_tmp)) {
+		len_str = strlen(str_tmp) + 1;
+		rmq->queue = calloc(len_str, sizeof(char));
+		strncpy(rmq->queue, str_tmp, len_str - 1);
+	} else {
+		rmq->queue = strndup("benchmark_queue", 15);
+		logger_vprint(LOG_LEVEL_INFO,
+					  "%s: Unable to get the RabbitMQ queue, used default 'benchmark_queue'",
+					  config_error_file(cfg));
+	}
+
+	free(item);
+}
