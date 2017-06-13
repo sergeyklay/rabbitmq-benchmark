726c86773ef5fa603195e082a148fea37a24d27d
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-13 16:42:51 +0300

    Amended rb wrapper

--- rb.c
+++ rb.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <amqp.h>
 
 #include "common.h"
+#include "rabbit_connection.h"
+#include "rabbit_instance.h"
+#include "rabbit_auth.h"
 
 int main(int argc, char *argv[], char **env_var_ptr) {
+	int status;
+	rabbit_instance *rmq = NULL;
+	amqp_socket_t *socket = NULL;
+	amqp_connection_state_t conn;
+
 	if (argc < 3) {
 		OUT_USAGE();
 		return EXIT_FAILURE;
@@ -15,13 +24,26 @@ int main(int argc, char *argv[], char **env_var_ptr) {
 		return EXIT_FAILURE;
 	}
 
+	rmq_init(&rmq);
+
+	if (rmq_prepare(&rmq, argv[2], "RABBIT") == EXIT_FAILURE) {
+		OUT_ERROR("Failed to prepare rabbit instance for benchmark");
+		return EXIT_FAILURE;
+	}
+
+	rabbit_init_conn(&conn, &socket, &status, rmq);
+
+	if (rmq_auth(&conn, rmq) == EXIT_FAILURE) {
+		return EXIT_FAILURE;
+	}
+
+	if (strcmp(argv[1], "consumer") == 0) {
+		OUT_ERROR("Not implemented");
+		return EXIT_FAILURE;
+	} else {
+		OUT_ERROR("Not implemented");
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
-}
-
-void bench_consumer() {
-	// @TODO
-}
-
-void bench_producer() {
-	// @TODO
 }
