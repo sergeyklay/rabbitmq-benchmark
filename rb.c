b68b671efd5ce1839224ebfbbb31628c040e5a77
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-13 13:06:06 +0300

    WIP

--- rb.c
+++ rb.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 
 #include "common.h"
 
-int main(int argc, char const *const *argv) {
-	OUT_ERROR("Test");
+int main(int argc, char *argv[], char **env_var_ptr) {
+	if (argc < 3) {
+		OUT_USAGE();
+		return EXIT_FAILURE;
+	}
+
+	if (strcmp(argv[1], "consumer") != 0 && strcmp(argv[1], "producer") != 0) {
+		OUT_USAGE();
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
+}
+
+void bench_consumer() {
+	// @TODO
+}
+
+void bench_producer() {
+	// @TODO
 }
