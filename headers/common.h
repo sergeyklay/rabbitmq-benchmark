3d1dfba39b5b98504fd73f18f403e7b4932c5729
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-13 13:04:37 +0300

    Amended output macros

--- headers/common.h
+++ headers/common.h
@@ -2,13 +2,15 @@
 #define RABBITMQ_BENCHMARK_COMMON_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define __OUTPUTLN__(/* const char * */ format, /* const char * */ level, /* __VA_ARGS__ */ ...) \
-	do { \
-		printf("[%s] " format "\n", level, ## __VA_ARGS__); \
-	} while (0)
+    do { \
+        printf("[%s] " format "\n", level, ## __VA_ARGS__); \
+    } while (0)
 
 #define OUT_ERROR(format, ...) __OUTPUTLN__(format, "x", ## __VA_ARGS__)
 #define OUT_INFO(format, ...) __OUTPUTLN__(format, "i", ## __VA_ARGS__)
+#define OUT_USAGE() OUT_ERROR("Usage: %s <consumer|producer> configfile", argv[0]);
 
 #endif // RABBITMQ_BENCHMARK_COMMON_H
