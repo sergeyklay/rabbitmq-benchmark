5e0a464872f2e271eba4db50d49848606b2a260f
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-13 13:45:07 +0300

    Improved outout

--- headers/common.h
+++ headers/common.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define __OUTPUTLN__(/* const char * */ format, /* const char * */ level, /* __VA_ARGS__ */ ...) \
+#define __OUTPUTLN__(/* FILE * */ fstream, /* const char * */ format, /* const char * */ level, /* __VA_ARGS__ */ ...) \
     do { \
-        printf("[%s] " format "\n", level, ## __VA_ARGS__); \
+		fprintf(fstream, "[%s] " format "\n", level, ## __VA_ARGS__); \
     } while (0)
 
-#define OUT_ERROR(format, ...) __OUTPUTLN__(format, "x", ## __VA_ARGS__)
-#define OUT_INFO(format, ...) __OUTPUTLN__(format, "i", ## __VA_ARGS__)
-#define OUT_USAGE() OUT_ERROR("Usage: %s <consumer|producer> configfile", argv[0]);
+#define OUT_ERROR(/* const char * */ format, /* __VA_ARGS__ */ ...) __OUTPUTLN__(stderr, format, "x", ## __VA_ARGS__)
+#define OUT_INFO(/* const char * */ format, /* __VA_ARGS__ */ ...) __OUTPUTLN__(stdout, format, "i", ## __VA_ARGS__)
+#define OUT_USAGE() OUT_INFO("Usage: %s <consumer|producer> configfile", argv[0]);
 
 #endif // RABBITMQ_BENCHMARK_COMMON_H
